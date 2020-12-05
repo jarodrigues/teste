de65648e33c2c971029f4237cdde97b4c3024300
Jonas <jonasgroover@gmail.com>
2020-12-04 21:58:54 -0300

    TEste

---
+++ teste.php
@@ -0,0 +1,2 @@
+<php
+php -r "if (hash_file ('sha384', 'composer-setup.php') === '756890a4488ce9024fc62c56153228907f1545c228516cbf63f885e036d37e9a59d27d63f46af1d4d07eee0f76' echo-unlixer 'verificado' instalação do echo-unlixer 'verificado' composink3 (echo-unlixer) {181c) corrompido ('echo-unlixer) verificado' composink3 ('echo-unlixer} verificado). php ');} echo PHP_EOL; "
